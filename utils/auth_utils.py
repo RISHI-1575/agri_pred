b698970645df4085e212a2827158402943618f1f
RISHI-1575 <23aiml050@bnmit.in>
2025-04-25 13:54:55 +0530

    Update auth_utils.py

--- utils/auth_utils.py
+++ utils/auth_utils.py
@@ -1,6 +1,13 @@
+import pandas as pd
+
 def login(username, password, role):
-    users = {
-        "farmer1": {"password": "1234", "role": "farmer"},
-        "company1": {"password": "admin", "role": "company"}
-    }
-    return users.get(username, {}).get("password") == password and users.get(username, {}).get("role") == role
+    try:
+        users = pd.read_csv("data/users.csv")
+        user = users[
+            (users["username"] == username) &
+            (users["password"] == password) &
+            (users["role"] == role)
+        ]
+        return not user.empty
+    except:
+        return False
